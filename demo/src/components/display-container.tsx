0635f50e885f48b5f60254ab04f976d1d5612a60
Tyson Skiba <tyson@skiba.com.au>
2021-09-12 21:35:33 +1000

    fix: remove unused code

--- demo/src/components/display-container.tsx
+++ demo/src/components/display-container.tsx
@@ -13,21 +13,6 @@ interface HeroProps {
   isCodeBlock?: boolean;
 }
 
-const demoCode = `import React from 'react';
-import { repositories } from 'react-weather-illustrations';
-
-const Icon: React.FC = () => {
-  const svg = repositories.fill.darksky.read('clear-night');
-  return <img src={svg} alt="weather icon" />;
-}
-
-export const Demo: React.FC = () => (
-  <React.Suspense fallback={<div>Loading</div>}>
-    <Icon />
-  </React.Suspense>
-)
-`;
-
 const DisplayContainerRoot = styled.div`
   display: flex;
   background: rgb(246, 249, 252);
